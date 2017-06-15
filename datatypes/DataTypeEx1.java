c2b358e4e2e80b70fb70ac0f58163ee95029fbb0
otabek kurbanov <otabek.kurbanov@yahoo.com>
2017-06-14 20:52:10 -0400

    June-14-8:51

---
+++ datatypes/DataTypeEx1.java
@@ -0,0 +1,14 @@
+class DataTypeEx1 
+{
+	public static void main(String[] args) 
+	{
+		int a=20;
+		int b=20;
+		int total= a+b;
+		boolean    result=b>a;
+		
+		System.out.println(result);
+		
+
+		}
+}
---
+++ datatypes/DataTypeEx3.java
@@ -0,0 +1,12 @@
+class DataTypeEx3
+{
+	public static void main(String[] args) 
+	{
+		int a=26425646;
+		System.out.println(a);
+		 final double PI=3.14;
+		 System.out.println(PI);
+		
+
+		}
+}
