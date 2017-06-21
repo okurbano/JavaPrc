975acb9b0cf3729afdbf6a904c1957727c47b801
otabek kurbanov <otabek.kurbanov@yahoo.com>
2017-06-20 21:29:12 -0400

    June-20-7:30

---
+++ control/IfEx1.java
@@ -0,0 +1,34 @@
+class IfEx1
+{
+	public static void main(String ar[])
+	{
+		int a=10;
+		int b=30;
+		int runs=90;
+		
+		if(runs >= 100)
+		{
+			System.out.println(" I made  century  "); 
+			System.out.println(" sharma");			
+			System.out.println("Thank you");
+		}
+		
+			if(runs<=50)				
+		{	
+			System.out.println(" I  made half-century");
+			System.out.println("Dhoni");
+			System.out.println("Bye");
+		}
+	}
+}
+
+
+
+/* (condition) expression can be in following way 
+	
+	 6 >  x   where x =10
+	 5   < 5  
+	 x  >  y 
+	 2  <= 6*2
+
+*/
