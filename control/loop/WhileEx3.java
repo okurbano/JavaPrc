1ca844700245d72ecf2517104479708b94781672
otabek kurbanov <otabek.kurbanov@yahoo.com>
2017-07-05 22:27:30 -0400

    whileloop-july-05

---
+++ control/loop/WhileEx3.java
@@ -0,0 +1,15 @@
+class WhileEx3
+{
+	public static void main(String ar[])
+	{
+		int i=10;
+
+		System.out.println((i/2));   //divident value when using divident operator     
+		System.out.println((i/20));   //when Numerator is less than denominator result= 0
+		System.out.println((i/11));   //divident 
+		System.out.println((i%2));  // modulus operator it will print remainder
+		System.out.println((i%20));   // for modulus when numerator is less than denominator result= numerator
+		System.out.println((i%11));     // for modulus when numerator is less than denominator result= numerator
+
+	}
+}
