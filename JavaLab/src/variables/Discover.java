6539d689abf39e05685b78527f70b4102f59dc93
otabek kurbanov <otabek.kurbanov@yahoo.com>
2017-07-11 23:06:47 -0400

    constructor-over-static-parameterized-jul-11

---
+++ JavaLab/src/variables/Discover.java
@@ -0,0 +1,14 @@
+package variables;
+
+import oops.Account;
+
+public class Discover {
+
+	public static void main(String[] args) {
+
+		Account a1= new Account();
+		a1.deposit(1010,"suresh",5000.0);
+		a1.display();
+	}
+
+}
